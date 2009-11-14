89d9ec8dd53346cb91ab2c263d750674b175bd20
Graham Brooks <graham@grahambrooks.com>
2009-11-13 18:15:09 -0600

    First passing test

---
+++ src/pilot/PilotApplication.java
@@ -0,0 +1,4 @@
+package pilot;
+
+public class PilotApplication {
+}
---
+++ test/pilot/PilotApplicationTests.java
@@ -0,0 +1,10 @@
+package pilot;
+
+import org.junit.Test;
+
+public class PilotApplicationTests {
+    @Test
+    public void canInstantiateAPilotApplication(){
+        new PilotApplication();
+    }
+}
